b5255ba1bb216f88b852ab8e0199d953c8ce2c0a
erzhqi <141692963+erzhqi@users.noreply.github.com>
2026-08-25 11:46:34 -0400

    Added linkages and removed visual pinky tracking

--- CameraProcessing.py
+++ CameraProcessing.py
@@ -13,11 +13,15 @@ HandLandmarker = mp.tasks.vision.HandLandmarker
 HandLandmarkerOptions = mp.tasks.vision.HandLandmarkerOptions
 VisionRunningMode = mp.tasks.vision.RunningMode
 
+mpHandsConnections = mp.solutions.hands.HAND_CONNECTIONS
+
 options = HandLandmarkerOptions(
     base_options = BaseOptions(
         model_asset_path="hand_landmarker.task",
         delegate = BaseOptions.Delegate.CPU),
     running_mode = VisionRunningMode.VIDEO,
+    num_hands = 1,
+    # min_hand_detection_confidence = 0.7 --> Threshold for initial hand detection
 )
 
 with HandLandmarker.create_from_options(options) as landmarker:
@@ -32,10 +36,23 @@ with HandLandmarker.create_from_options(options) as landmarker:
 
             if mpResult.hand_landmarks:
                 for handLandmarks in mpResult.hand_landmarks:
-                    for landmark in handLandmarks:
-                        x = int(landmark.x * frame.shape[1])
-                        y = int(landmark.y * frame.shape[0])
-                        cv2.circle(frame, (x, y), 5, (0, 255, 0), -1)
+                    points = []
+                    for index, landmark in enumerate(handLandmarks): # Ignores pinky finger (17-20)
+                        if index <= 16:
+                            x = int(landmark.x * frame.shape[1])
+                            y = int(landmark.y * frame.shape[0])
+                            points.append((x, y))
+                            cv2.circle(frame, (x, y), 7, (0, 0, 255), -1)
+                            print(points[0])
+                            print(points[0][0])
+                            print(points[0][1])
+
+                    pinkyIndices = {17, 19, 19, 20}
+                    for connection in mpHandsConnections:
+                        start, end = connection
+                        if start in pinkyIndices or end in pinkyIndices:
+                            continue
+                        cv2.line(frame, points[start], points[end], (0, 255, 0), 1)
 
             cv2.imshow("Behold", frame)
             if cv2.waitKey(1) == 113:
