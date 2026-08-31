06219996e1bb22847a27aa36182d01d395d040e7
erzhqi <141692963+erzhqi@users.noreply.github.com>
2026-08-31 11:06:01 -0400

    Commenting and cleaning up code

--- CameraProcessing.py
+++ CameraProcessing.py
@@ -1,6 +1,15 @@
+"""
+    CameraProcessing.py
+    Erik Zhu
+    August 31, 2026
+    Vision side of this project. Uses OpenCV to read frames from a webcam and
+    runs the MediaPipe Gesture Recognizer landmark to compute bend percentages
+    for each finger. These values are then sent as a comma separated string to the 
+    ESP32 over serial so the AmazingHand can mimic the movements of my hand.
+"""
 import cv2
 import mediapipe as mp
-import time
+import time # For timestampping frames
 import serial
 import serial.tools.list_ports
 
@@ -10,12 +19,14 @@ motorStatus = "Motors: OFF"
 textColor = (0, 0, 255)
 pinkyIndices = {17, 18, 19, 20}
 
-BAUD_RATE = 115200
+BAUD_RATE = 115200 # (ESP: 115200, Arduino: 115200 OR 9600 in some cases)
 
+# Setting up video capture object
 videoCapture = cv2.VideoCapture(0)
 videoCapture.set(cv2.CAP_PROP_FRAME_WIDTH, 600)
 videoCapture.set(cv2.CAP_PROP_FRAME_HEIGHT, 600)
 
+# Configuring options for MediaPipe and the gesture recognizer task
 BaseOptions = mp.tasks.BaseOptions
 GestureRecognizer = mp.tasks.vision.GestureRecognizer
 GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
@@ -26,50 +37,59 @@ mpHandsConnections = mp.solutions.hands.HAND_CONNECTIONS
 options = GestureRecognizerOptions(
     base_options = BaseOptions(
         model_asset_path="gesture_recognizer.task",
-        delegate = BaseOptions.Delegate.CPU),
+        delegate = BaseOptions.Delegate.CPU), # Have to manually delegate CPU space for MacOS,
     running_mode = VisionRunningMode.VIDEO,
-    num_hands = 1,
-    # min_hand_detection_confidence = 0.7 --> Threshold for initial hand detection
+    num_hands = 1 # Only allows for tracking one hand at once,
 )
 
+# Searching for appropriate port for serial
 def find_port():
     ports = serial.tools.list_ports.comports()
     for port in ports:
-        if "usbserial" in port.device.lower():
+        if "usbserial" in port.device.lower(): # MacOS specific format for ports
             return port.device
     return None
 
 devicePort = find_port()
 print(f"Port Found: {devicePort}")
+# Setting up the ESP32 port 
 esp32 = serial.Serial(devicePort, BAUD_RATE, timeout=2)
 
+# Creating a recognizer object with our configured options
 with GestureRecognizer.create_from_options(options) as recognizer:
+    # Runs a loop while the camera is on
     while videoCapture.isOpened():
+        # Returns a variable "isRead" if the camera is reading, as well as the current frame (NumPy array)
         isRead, frame = videoCapture.read()
+        # Motor status
         cv2.putText(frame, motorStatus, (50, 50), cv2.FONT_HERSHEY_COMPLEX, 1, textColor, 2, cv2.LINE_AA)
-        if isRead:
+        if isRead: # Runs if the camera is working
             RGBFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # Converting from BGR (OpenCV) to RGB (Mediapipe)
             mpFrame = mp.Image(image_format=mp.ImageFormat.SRGB, data=RGBFrame)
-            frameTimestamp = int((time.time() - startTime) * 1000)
-            mpResult = recognizer.recognize_for_video(mpFrame, frameTimestamp)
-            if mpResult.hand_landmarks:
+            frameTimestamp = int((time.time() - startTime) * 1000) # Timestamp associated with a frame
+            mpResult = recognizer.recognize_for_video(mpFrame, frameTimestamp) 
+            if mpResult.hand_landmarks: # If hand(s) are detected in a given frame
                 for handIndex, handLandmarks in enumerate(mpResult.hand_landmarks):
                     points = []
                     for index, landmark in enumerate(handLandmarks): # Ignores pinky finger (17-20)
                         if index <= 16:
                             x = int(landmark.x * frame.shape[1])
                             y = int(landmark.y * frame.shape[0])
-                            points.append((x, y))
+                            points.append((x, y)) # Adding coordinates of each keypoint on hand to array
                             # cv2.circle(frame, (x, y), 7, (0, 0, 255), -1)
+                            
+                            # Printing keypoint number next to each keypoint
                             cv2.putText(frame, str(index), (x+7, y+2), cv2.FONT_HERSHEY_COMPLEX, 0.6, (255, 0, 0), 2, cv2.LINE_AA)
 
+                    # Drawing in each connection between keypoints
                     for connection in mpHandsConnections:
                         start, end = connection
+                        #Ignoring connections that involve the pinky finge
                         if start in pinkyIndices or end in pinkyIndices:
                             continue
                         cv2.line(frame, points[start], points[end], (0, 255, 0), 1)
 
-                    if not motorsActive:
+                    if not motorsActive: # Initial code to detect an "Open_Palm" gesture by the right hand
                         whichHand = mpResult.handedness[0][0].category_name
                         whichGesture = mpResult.gestures[0][0].category_name
                         gestureScore = mpResult.gestures[0][0].score
@@ -79,43 +99,46 @@ with GestureRecognizer.create_from_options(options) as recognizer:
                             motorStatus = "Motors: ON"
                             textColor = (0, 255, 0)
                             esp32.write(b"Motors Active!\n")
+
+                            # Initializing a maximum distance between keypoints to serve as the baseline for
+                            # a completely open hand
                             maxDistIndexf = int((((points[8][0]-points[5][0])**2 + (points[8][1]-points[5][1])**2)**0.5)) / 2
                             maxDistMiddlef = int((((points[12][0]-points[9][0])**2 + (points[12][1]-points[9][1])**2)**0.5)) / 2
                             maxDistRingf = int((((points[16][0]-points[13][0])**2 + (points[16][1]-points[13][1])**2)**0.5)) / 2
                             maxDistThumb = int((((points[4][0]-points[0][0])**2 + (points[4][1]-points[0][1])**2)**0.5)) / 2
 
                     if motorsActive:
+                        # Calculating current distance between keypoints
                         distanceIndexf = int((((points[8][0]-points[5][0])**2 + (points[8][1]-points[5][1])**2)**0.5)) / 2
                         distanceMiddlef = int((((points[12][0]-points[9][0])**2 + (points[12][1]-points[9][1])**2)**0.5)) / 2
                         distanceRingf = int((((points[16][0]-points[13][0])**2 + (points[16][1]-points[13][1])**2)**0.5)) / 2
                         distanceThumb = int((((points[4][0]-points[0][0])**2 + (points[4][1]-points[0][1])**2)**0.5)) / 2
 
+                        # Comparing to maximum distance and calculating a bend percentage
                         percentBendIndex = round((maxDistIndexf - distanceIndexf) / maxDistIndexf, 2) 
                         percentBendMiddle = round((maxDistMiddlef - distanceMiddlef) / maxDistMiddlef, 2) 
                         percentBendRing = round((maxDistRingf - distanceRingf) / maxDistRingf, 2) 
-                        percentBendThumb = round((maxDistThumb - distanceThumb) / maxDistThumb, 2) * 3
-
-                        if percentBendThumb > 1:
-                            percentBendThumb = 1
+                        percentBendThumb = round((maxDistThumb - distanceThumb) / maxDistThumb, 2)
 
                         fingerBendList = [percentBendIndex, percentBendMiddle, percentBendRing, percentBendThumb]
                         motorBend = ""
                         for i in range(len(fingerBendList)):
-                            if fingerBendList[i] < 0:
+                            if fingerBendList[i] < 0: # Normalizing values to 0 if they become negative
                                 fingerBendList[i] = 0
-                            motorBend += str(fingerBendList[i]) + ","
+                            motorBend += str(fingerBendList[i]) + "," # Adding to string that is sent to ESP32
                         esp32.write((motorBend + "\n").encode("utf-8"))
                         cv2.putText(frame, "Index Bend: " + str(fingerBendList[0]*100) + "%", (50, 100), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
                         cv2.putText(frame, "Middle Bend: " + str(fingerBendList[1]*100) + "%", (50, 150), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
                         cv2.putText(frame, "Ring Bend: " + str(fingerBendList[2]*100) + "%", (50, 200), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
                         cv2.putText(frame, "Thumb Bend: " + str(fingerBendList[3]*100) + "%", (50, 250), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
-
-            cv2.imshow("Behold", frame)
-            if cv2.waitKey(1) == 113:
+            # Display camera window
+            cv2.imshow("Amazing Hand Mimic", frame)
+            if cv2.waitKey(1) == 113: # Exits if 'q' is pressed
                 break
         else:
             print("Failed to Read")
             break
 
+# Destroying the videoCapture instance and stops execution of OpenCV
 videoCapture.release()
 cv2.destroyAllWindows()
--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,41 @@
-#include <Arduino.h>
-#include <SCServo.h>
+/*
+    main.cpp
+    Erik Zhu
+    August 31, 2026
+    Firmware to control the AmazingHand's servos through a FE-URT-2 bus. The code receives 
+    comma separated bend percentages from CameraProcessing.py and writes them to each finger
+    via SyncWritePos to match movements of my hand.
 
-// Two independent servo communication buses for arm and hand
+*/
+
+#include <Arduino.h>
+#include <SCServo.h> //Library for controlling Feetech STS & SMS series servos
+
 SMS_STS handServo;
-SMS_STS armServo;
 
 #define HAND_RX_PIN 13
 #define HAND_TX_PIN 14
 
-#define ARM_RX_PIN 26
-#define ARM_TX_PIN 27
+HardwareSerial handSerial(1); // Setting up serial for communication with motors via a FE-URT-2
 
-// Two serial buses for hand and arm
-HardwareSerial handSerial(1);
-HardwareSerial armSerial(2);
-
-// Setting constants for each motor
-const uint8_t FINGER1_RID = 1;
-const uint8_t FINGER1_LID = 2;
-const uint8_t FINGER2_RID = 3;
-const uint8_t FINGER2_LID = 4;
-const uint8_t FINGER3_RID = 5;
-const uint8_t FINGER3_LID = 6;
-const uint8_t THUMB_LID = 7; // This is the one that is "reversed"
-const uint8_t THUMB_RID = 8;
-
-const uint8_t ARM1 = 1;
-const uint8_t ARM2 = 2;
-const uint8_t ARM3 = 3;
-const uint8_t ARM4 = 4;
-const uint8_t ARM5 = 5;
-
-uint8_t hand_servo_list[] = {FINGER1_RID, FINGER1_LID, FINGER2_RID, FINGER2_LID, FINGER3_RID, FINGER3_LID, THUMB_LID, THUMB_RID};
-const uint8_t NUM_HAND_SERVOS = sizeof(hand_servo_list);
-
-uint8_t arm_servo_list[] = {ARM1, ARM2, ARM3, ARM4, ARM5};
-const uint8_t NUM_ARM_SERVOS = sizeof(arm_servo_list);
-
-uint8_t INDEX[2] = {1, 2};
-uint8_t MIDDLE[2] = {3, 4};
-uint8_t RING[2] = {5, 6};
-uint8_t THUMB[2] = {7, 8};
+// Two motors per finger
+// uint8_t INDEX[2] = {1, 2};
+// uint8_t MIDDLE[2] = {3, 4};
+// uint8_t RING[2] = {5, 6};
+// uint8_t THUMB[2] = {7, 8};
 
 uint8_t ALL_FINGERS[8] = {1, 2, 3, 4, 5, 6, 7, 8};
 
-int16_t INDEX_POSITION_CLOSE[2] = {3044, 1038};
-int16_t INDEX_POSITION_OPEN[2] = {1875, 2183};
-  
-int16_t MIDDLE_POSITION_CLOSE[2] = {3184, 1101};
-int16_t MIDDLE_POSITION_OPEN[2] = {1926, 2169};
-
-int16_t RING_POSITION_CLOSE[2] = {3176, 835};
-int16_t RING_POSITION_OPEN[2] = {2040, 2108};
-
-int16_t THUMB_POSITION_CLOSE[2] = {3191, 1002};
-int16_t THUMB_POSITION_OPEN[2] = {1978, 2370};
-
-uint16_t SPEED[2] = {1000, 1000};
-uint8_t ACCELERATION[2] = {50, 50};
+uint16_t SPEED[8] = {1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000};
+uint8_t ACCELERATION[8] = {50, 50, 50, 50, 50, 50, 50, 50};
 
 String pythonText;
 boolean motorsOn = false;
 double bendPercentages[4];
 
-void ping_servos();
-void finger_movement_test(int index);
 void parseValues(String originalString, double bendValues[4]);
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(115200);
   delay(1000);
 
@@ -76,126 +43,38 @@ void setup() {
   handSerial.begin(1000000, SERIAL_8N1, HAND_RX_PIN, HAND_TX_PIN);
   handServo.pSerial = &handSerial;
   delay(200);
-
-  // Initializing serial for arm
-  armSerial.begin(1000000, SERIAL_8N1, ARM_RX_PIN, ARM_TX_PIN);
-  armServo.pSerial = &armSerial;
-  delay(200);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Repeatedly reading from serial
   if (Serial.available() > 0){
     pythonText = Serial.readStringUntil('\n');
     pythonText.trim();
-    if (pythonText == "Motors Active!"){
+    if (pythonText == "Motors Active!"){ // Activate motors
       motorsOn = true;
       Serial.println("Motors Active");
     }
     else if (motorsOn){
+      // After motors are activated, parse values sent from python script and write values to servo
       Serial.println(pythonText);
-      parseValues(pythonText, bendPercentages);
+      parseValues(pythonText, bendPercentages); // Writing bend percentages to array
       int16_t HAND_BEND_POSITIONS[8] = {
         1875+round(1169*bendPercentages[0]), 2183-round(1145*bendPercentages[0]),
         1926+round(1258*bendPercentages[1]), 2169-round(1068*bendPercentages[1]),
         2040+round(1136*bendPercentages[2]), 2108-round(1273*bendPercentages[2]),
         1978+round(1213*bendPercentages[3]), 2370-round(1368*bendPercentages[3])
       };
+      // Synchronously writing values to all motors
       handServo.SyncWritePosEx(ALL_FINGERS, 8, HAND_BEND_POSITIONS, SPEED, ACCELERATION);
       }
     }
 }
 
+// Parsing from a string in the format "num1,num2,num3,num4," into an array of doubles (eg. {0.67, 1, 0.06, 0.07})
 void parseValues(String originalString, double bendValues[4]){
   for (int i = 0; i < 4; i ++){
     int indexComma = originalString.indexOf(',');
-    bendValues[i] = (originalString.substring(0, indexComma)).toDouble();
-    originalString = originalString.substring(indexComma+1);
-  }
-}
-
-void servo_check(){
-  Serial.println("Checking HAND Servos:");
-  for (int i = 0; i < NUM_HAND_SERVOS; i++){
-    int pingResult = handServo.Ping(hand_servo_list[i]);
-    if (pingResult != -1){
-      Serial.println("Response from Servo " + String(hand_servo_list[i]));
-      int finger_pos = handServo.ReadPos(hand_servo_list[i]);
-      Serial.println("Finger Position: " + String(finger_pos));
-      Serial.println("-------");
-    }
-    else{
-      Serial.println("No Response from Servo " + String(hand_servo_list[i]));
-    }
-  }
-  Serial.println("*******");
-  Serial.println("Checking ARM Servos:");
-  for (int i = 0; i < NUM_ARM_SERVOS; i++){
-    int pingResult = armServo.Ping(arm_servo_list[i]);
-    if (pingResult != -1){
-      Serial.println("Response from Servo " + String(arm_servo_list[i]));
-      int arm_pos = armServo.ReadPos(arm_servo_list[i]);
-      Serial.println("Arm Motor Position: " + String(arm_pos));
-      Serial.println("-------");
-    }
-    else{
-      Serial.println("No Response from Servo " + String(arm_servo_list[i]));
-    }
-  }
-}
-
-void finger_movement_test(int index){  
-  Serial.println("Beginning Positions:");
-  if (index == 0){
-    Serial.println(handServo.ReadPos(1));
-    Serial.println(handServo.ReadPos(2));
-    Serial.println("Beginning to move finger");
-    handServo.SyncWritePosEx(INDEX, 2, INDEX_POSITION_OPEN, SPEED, ACCELERATION);
-    delay(2000);
-    Serial.println("Finger finished moving");
-    handServo.SyncWritePosEx(INDEX, 2, INDEX_POSITION_CLOSE, SPEED, ACCELERATION);
-    delay(2000);
-    Serial.println("Ending Positions:");
-    Serial.println(handServo.ReadPos(1));
-    Serial.println(handServo.ReadPos(2));
-  }
-  else if (index == 1){
-    Serial.println(handServo.ReadPos(3));
-    Serial.println(handServo.ReadPos(4));
-    Serial.println("Beginning to move MIDDLE finger");
-    handServo.SyncWritePosEx(MIDDLE, 2, MIDDLE_POSITION_OPEN, SPEED, ACCELERATION);
-    delay(2000);
-    Serial.println("MIDDLE finger finished moving");
-    handServo.SyncWritePosEx(MIDDLE, 2, MIDDLE_POSITION_CLOSE, SPEED, ACCELERATION);
-    delay(2000);
-    Serial.println("Ending Positions:");
-    Serial.println(handServo.ReadPos(3));
-    Serial.println(handServo.ReadPos(4));
-  }
-  else if (index == 2){
-    Serial.println(handServo.ReadPos(5));
-    Serial.println(handServo.ReadPos(6));
-    Serial.println("Beginning to move RING finger");
-    handServo.SyncWritePosEx(RING, 2, RING_POSITION_OPEN, SPEED, ACCELERATION);
-    delay(2000);
-    Serial.println("RING finger finished moving");
-    handServo.SyncWritePosEx(RING, 2, RING_POSITION_CLOSE, SPEED, ACCELERATION);
-    delay(2000);
-    Serial.println("Ending Positions:");
-    Serial.println(handServo.ReadPos(5));
-    Serial.println(handServo.ReadPos(6));
-  }
-  else if (index == 3){
-    Serial.println(handServo.ReadPos(7));
-    Serial.println(handServo.ReadPos(8));
-    Serial.println("Beginning to move THUMB");
-    handServo.SyncWritePosEx(THUMB, 2, THUMB_POSITION_OPEN, SPEED, ACCELERATION);
-    delay(2000);
-    Serial.println("THUMB finished moving");
-    handServo.SyncWritePosEx(THUMB, 2, THUMB_POSITION_CLOSE, SPEED, ACCELERATION);
-    delay(2000);
-    Serial.println("Ending Positions:");
-    Serial.println(handServo.ReadPos(5));
-    Serial.println(handServo.ReadPos(6));
+    bendValues[i] = (originalString.substring(0, indexComma)).toDouble(); // Take a substring up until a comma
+    originalString = originalString.substring(indexComma+1); // Modifying the original string
   }
 }
